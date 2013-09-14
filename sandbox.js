74c45295b4b95834e9aa3ab5ac9061e38573c0e0
Jay Hannah <jay.hannah@iinteractive.com>
2013-09-14 14:06:20 -0500

    .

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,51 @@
+
 // http://fightcodegame.com/robots/update/22116/
 
-var Robot = function(robot) {
-};
+var Robot = function(robot) { };
 
 var jstate = {};
 jstate.tick = 0;
 jstate.lastEnemySeenAbsoluteAngle = 0;
 jstate.nextcannonAbsoluteAngle = 0;
-jstate.sweepWidth = 1;
+jstate.sweepDirection = 1;
+jstate.offby = 0;
 jstate.moveOrNot = 1;
+jstate.logcount = 0;
 
 Robot.prototype.onIdle = function(ev) {
   // ev.robot.clone();
   jstate.tick = jstate.tick + 1;
-  ev.robot.rotateCannon(jstate.sweepWidth);
-  if (jstate.lastEnemySeenAbsoluteAngle) { 
-    var offby = ev.robot.cannonAbsoluteAngle - jstate.lastEnemySeenAbsoluteAngle; 
-    if (jstate.tick < 50) {
-      console.log("JAY0 [" + offby + "|" + JSON.stringify(jstate) + "]");
-    }
-    if(offby > jstate.sweepWidth) {
-      jstate.sweepWidth *= -2;
-    }
+  logit("JAY0");
+  if (jstate.lastEnemySeenAbsoluteAngle) {
+    
   } else {
-    ev.robot.rotateCannon(1); 
+     jstate.nextcannonAbsoluteAngle = ev.robot.cannonAbsoluteAngle;
   }
+  if (ev.robot.cannonAbsoluteAngle > jstate.nextcannonAbsoluteAngle) {
+    jstate.offby = ev.robot.cannonAbsoluteAngle - jstate.nextcannonAbsoluteAngle;
+  } else {
+    jstate.offby = jstate.nextcannonAbsoluteAngle - ev.robot.cannonAbsoluteAngle;
+  }
+  if (offby.abs < 2) { 
+    jstate.nextcannonAbsoluteAngle = jstate.lastEnemySeenAbsoluteAngle 
+      - offby * 2 * jstate.sweepDirection;
+    ev.robot.rotateCannon(offby * jstate.sweepDirection);
+    jstate.sweepDirection *= -1;
+  } 
+  ev.robot.rotateCannon(jstate.sweepDirection);
 //  if (jstate.moveOrNot++ % 7) {
 //     ev.robot.ahead(10);
 //  }
 //  ev.robot.turn(10);
-
 };
 
+function logit(msg) {
+  if (jstate.logcount < 20) {
+    console.log(msg + " " + JSON.stringify(jstate));
+    jstate.logcount = jstate.logcount + 1; 
+  }
+}
+  
 // this method gets called whenever we hit another robot...
 Robot.prototype.onRobotCollision = function(ev) {
   ev.robot.back(10);
@@ -53,8 +67,7 @@ Robot.prototype.onScannedRobot = function(ev) {
   ) { return }
   ev.robot.fire();
   jstate.lastEnemySeenAbsoluteAngle = ev.robot.cannonAbsoluteAngle;
-  jstate.nextcannonAbsoluteAngle =    ev.robot.cannonAbsoluteAngle;
-  jstate.sweepWidth = 1;
+  jstate.nextcannonAbsoluteAngle =    ev.robot.cannonAbsoluteAngle + 1;
   // console.log("JAY0 " + jstate.nextcannonAbsoluteAngle);
     //ev.robot.log(ev.robot.position.x);
 };
@@ -68,6 +81,5 @@ Robot.prototype.onHitByBullet = function(ev) {
 
   //robot.turn(90 - ev.bulletBearing);
 };
-
 
 
